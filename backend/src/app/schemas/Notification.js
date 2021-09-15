e2835b1827b75de781b8d349e9fe46214b2a778b
dihgo01 <49100518+dihgo01@users.noreply.github.com>
2021-09-15 08:51:17 -0300

    Add files via upload

--- backend/src/app/schemas/Notification.js
+++ backend/src/app/schemas/Notification.js
@@ -1,22 +1,24 @@
 import mongoose from 'mongoose';
 
-const NotificationSchema = new mongoose.Schema({
+const NotificationShema = new mongoose.Schema(
+  {
     content: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    user:{
-        type: Number,
-        required:true,
+    user: {
+      type: Number,
+      required: true,
     },
-    read:{
-        type: Boolean,
-        required: true,
-        default: false,
+    read: {
+      type: Boolean,
+      required: true,
+      default: false,
     },
-},
-{
-    timestamps:true,
-})
+  },
+  {
+    timestamps: true,
+  },
+);
 
-export default mongoose.model('Notification', NotificationSchema);
+export default mongoose.model('Notification', NotificationShema);
--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,12 +1,12 @@
 export default {
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    secure: false,
-    auth: {
-      user: "2b0d0da0d8570f",
-      pass: "b0464391aef095"
-    },
-    default: {
-        from:'Equipe GoBarber <noreply@gobarber.com>'
-    },
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
 };
